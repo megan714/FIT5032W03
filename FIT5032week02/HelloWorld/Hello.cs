04ea19bec87d355046128a1dec13d2812f828784
1132015273@qq.com <1132015273@qq.com>
2020-07-21 12:09:20 +0800

    添加项目文件。

---
+++ FIT5032week02/HelloWorld/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032week02.HelloWorld
+{
+    public class Hello
+    {
+        public String GetHello()
+        {
+            return "Hello World!";
+        }
+    }
+}
